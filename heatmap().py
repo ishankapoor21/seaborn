64a57456f351374fb3d9259a70fffc915f90952c
ishan_kapoor <64485253+ishankapoor21@users.noreply.github.com>
2020-04-29 00:15:03 +0530

    Add files via upload

---
+++ heatmap().py
@@ -0,0 +1,7 @@
+#seaborn_heatmap_use
+
+import seaborn as sns
+dataset=sns.load_dataset('tips')    #Load a dataset from the online repository (requires internet)
+dataset.head()                              # Return the first `n` rows , n : int, default 5
+plot=sns.heatmap(dataset.corr()) # heatmap() -> Plot rectangular data as a color-encoded matrix.
+                                                   # corr() -> Compute pairwise correlation of                                                                #               columns, excluding NA/null values
